ec97504e8b27be0694dab001d0097f24ad88b260
suryadwd <surajdwivedi644@gmail.com>
2025-06-14 13:19:04 +0530

    socket

--- server/server.js
+++ server/server.js
@@ -2,13 +2,27 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import { connectDb } from "./config/clouddb.js";
 import cors from "cors";
 app.use(cors());
-
-import { connectDb } from "./config/clouddb.js";
-
-
+import { Server } from "socket.io";
+import { createServer } from "http";
 let PORT = process.env.PORT || 3000;
+
+const io = new Server(createServer(app));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  console.log("Id: ", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    console.log("Id: ", socket.id);
+  });
+
+})
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
